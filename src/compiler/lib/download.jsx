561ffcef0ee85958138262e8b2b295acfa2cdc29
Jaid <jaid.jsx@gmail.com>
2018-05-07 16:14:55 +0200

    Added curl option

--- src/compiler/lib/download.jsx
+++ src/compiler/lib/download.jsx
@@ -1,17 +1,33 @@
 import shellEscape from "../lib/shellEscape"
 
 exports.toStream = (setup, url, handlerScript) => {
-    if (setup.format === "long") {
-        return shellEscape`wget --quiet --output-document - ${url} | ` + handlerScript
+    if (setup.downloadTool === "curl") {
+        if (setup.format !== "long") {
+            return shellEscape`curl --silent --fail --location --retry 3 ${url} | ` + handlerScript
+        } else {
+            return shellEscape`curl -fsL --retry 3 ${url} | ` + handlerScript
+        }
     } else {
-        return shellEscape`wget -qO- ${url} | ` + handlerScript
+        if (setup.format === "long") {
+            return shellEscape`wget --tries 3 --quiet --output-document - ${url} | ` + handlerScript
+        } else {
+            return shellEscape`wget -t 3 -qO- ${url} | ` + handlerScript
+        }
     }
 }
 
 exports.toFile = (setup, url, file) => {
-    if (setup.format === "long") {
-        return shellEscape`wget --output-document ${file} ${url}`
+    if (setup.downloadTool === "curl") {
+        if (setup.format === "long") {
+            return shellEscape`curl --fail --location --progress-bar --retry 3 --output ${file} ${url}`
+        } else {
+            return shellEscape`curl -fLo ${file} --retry 3 ${url}`
+        }
     } else {
-        return shellEscape`wget -O ${file} ${url}`
+        if (setup.format === "long") {
+            return shellEscape`wget --tries 3 --output-document ${file} ${url}`
+        } else {
+            return shellEscape`wget -t 3 -O ${file} ${url}`
+        }
     }
 }
