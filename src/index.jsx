6eb14709b840d534d0a4d3f29dec2a68cc9ed3b3
Jaid <jaid.jsx@gmail.com>
2018-05-12 14:07:56 +0200

    Added react-ga

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 import ReactDom from "react-dom"
+import ReactGa from "react-ga"
 import App from "components/App"
 import "./theme.global.scss"
+
+if (process.env.NODE_ENV === "production") {
+    ReactGa.initialize("UA-51563406-5");
+    ReactGa.pageview(window.location.pathname + window.location.search);
+    console.log(2)
+}
 
 ReactDom.render(<App />, document.getElementById("react"))
